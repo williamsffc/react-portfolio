7d29222eb6b32e3560ed259db985b026f0bae708
William Flores <williamsffc@gmail.com>
2020-10-09 17:49:37 -0700

    fixing deployment

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { Portfolio } from "./Pages/Portfolio";
 import { About } from "./Pages/About";
-import { Layout } from "./components/Layout";
 import { Contact } from "./Pages/Contact";
 import { NoMatch } from "./Pages/NoMatch";
+import { Layout } from "./components/Layout";
 import { NavBar } from "./components/NavBar";
 import { Jumbotron } from "./components/Jumbotron";
 import { Footer } from "./components/Footer";
@@ -17,10 +17,14 @@ class App extends Component {
         <NavBar />
         <Jumbotron />
         <Layout>
-          <Router>
+          <Router basename={process.env.PUBLIC_URL}>
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route exact path="/portfolio" component={Portfolio} />
+              <Route
+                exact
+                path="/portfolio"
+                component={Portfolio}
+              />
               <Route exact path="/about" component={About} />
               <Route exact path="/contact" component={Contact} />
               <Route component={NoMatch} />
