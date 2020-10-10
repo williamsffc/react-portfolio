29729fd3da6f0b48005a6a6241bc20e2e649163e
William Flores <williamsffc@gmail.com>
2020-10-09 17:36:39 -0700

    Updates

---
+++ precache-manifest.4881297b92530ba15aba3d257bce011a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a3033406995cd2bc02a50df2a0e655",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d24c3d7b84971e20d6d9",
+    "url": "/static/js/2.d1fcc004.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d1fcc004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf02f68cb972d0eb728d",
+    "url": "/static/js/main.273650ef.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "54567aa08dfbb0fbb6c637756b74ebf4",
+    "url": "/static/media/AboutMe.54567aa0.jpg"
+  },
+  {
+    "revision": "61e1a17787f41195af242c4ecc3a2eb8",
+    "url": "/static/media/HW10-Team_Profile_Generator.61e1a177.jpg"
+  },
+  {
+    "revision": "f9d9686544cc6f76672c11879c70a4c8",
+    "url": "/static/media/HW11-Note_Taker.f9d96865.jpg"
+  },
+  {
+    "revision": "a8467c1eb4f59e1d45aa52e1bd278ebb",
+    "url": "/static/media/HW12-Employee_Management_System.a8467c1e.jpg"
+  },
+  {
+    "revision": "6882c5113065110806909256d2c52e1b",
+    "url": "/static/media/HW13-Burger_Logger.6882c511.PNG"
+  },
+  {
+    "revision": "e326712ed225cd92e934c31e98651889",
+    "url": "/static/media/Navbar-bg.e326712e.jpg"
+  },
+  {
+    "revision": "0f8a62a3b99181a86c911a4773e4e993",
+    "url": "/static/media/Project_2.0f8a62a3.png"
+  },
+  {
+    "revision": "b71410fcaef81acb873b62df4b9b7902",
+    "url": "/static/media/Project_3.b71410fc.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.0332dedcd7d3411e43dd8ffb1ba79386.js"
+  "/precache-manifest.4881297b92530ba15aba3d257bce011a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
