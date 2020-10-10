4de9e708e482ea5a52e1c36dd23e023abe6ce5e7
William Flores <williamsffc@gmail.com>
2020-10-09 17:40:32 -0700

    fixing deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
@@ -9,3 +10,5 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+
+registerServiceWorker();
